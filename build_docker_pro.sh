3059c660d4006a7d0e49ddb24aa5ee63719b33a1
defxp10 <defx110@gmail.com>
2024-04-16 13:25:01 +0800

    feat: docker file

---
+++ build_docker_pro.sh
@@ -0,0 +1,2 @@
+gf build -s linux -a amd64
+docker build -t xxxx/eth-log-scanner  .
---
+++ scanner.sh
@@ -0,0 +1,1 @@
+docker run -d -v ./manifest:/app/manifest -v ./config:/app/config  --name log-scanner  xxxx/eth-log-scanner /app/main
